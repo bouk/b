7805f13bf9a8832ce6fe22814e131a98d1c24e90
Bouke van der Bijl <i@bou.ke>
2020-06-18 11:28:02 +0200

    Go way into the deep by adding some PATHS
    This makes sure libs work, and I can compile nokogiri outside the nix
    garden.

--- default.nix
+++ default.nix
@@ -81,6 +81,7 @@ let
     bat
     boukeBin
     cargo
+    clang
     cmake
     coreutils
     curl
@@ -104,6 +105,9 @@ let
     jq
     kind
     kubectl
+    libiconv
+    libxml2
+    libxslt
     luajit
     mockgen
     mysql80
@@ -127,8 +131,28 @@ let
     xz
     yarn
     youtube-dl
+    zlib
   ];
-  boukeFish = (pkgs.callPackage ./fish.nix { extraConfig = "set -gxp PATH ${pkgs.lib.makeBinPath paths}"; });
+  env = pkgs.buildEnv {
+    name = "bouke";
+    paths = paths;
+    extraOutputsToInstall = [ "bin" "dev" "lib" ];
+  };
+  # NOTE: here I am doing what nix-shell does with NIX_LDFLAGS and stuff.
+  # I am adding all the libraries that are put into the env (like libxml2)
+  # into the compiler paths.
+  # This means I don't have to use a nix-shell to do basic stuff like build nokogiri.
+  # This means of course that I'm using this beautiful nix system to have reproducible stuff,
+  # only to throw it all into the global environment.
+  # I just want my set-up to be reproducible though.
+  # When I build stuff using the tools in my setup, it can just be a mess and that's fine.
+  boukeFish = (pkgs.callPackage ./fish.nix {
+    extraConfig = ''
+      set -gxp CPATH ${pkgs.lib.makeSearchPathOutput "dev" "include" [ env ] }
+      set -gxp LIBRARY_PATH ${pkgs.lib.makeLibraryPath [ env ] }
+      set -gxp PATH ${pkgs.lib.makeBinPath [ env ] }
+      '';
+  });
   package = boukeFish;
 in
   if pkgs.lib.inNixShell
